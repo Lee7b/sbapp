3eb2d11f48b91c796bf3c31b555afa9f6928bf62
Sam Burkett <sburk377@gmail.com>
2019-08-30 17:02:59 -0500

    Fixes

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -16,8 +16,7 @@ class Home extends React.Component {
     AOS.init({
       duration : 2000,
       offset : -110,
-      once : true,
-      disable : true
+      once : true
     })
   }
     render () {
