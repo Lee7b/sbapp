d7beffa9903144f24a2c4980c1e74c31e50d687d
Sam Burkett <sburk377@gmail.com>
2019-08-30 13:30:29 -0500

    Updated project page

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -12,20 +12,23 @@ class Projects extends React.Component {
               <MDBCard style={{width:"500px"}}>
                 <MDBCardHeader><strong>sbappv3</strong></MDBCardHeader>
                 <MDBCardBody>
-                  <MDBCardText>My personal website. ReactJS front-end with node/express back-end. Much better than the last one!</MDBCardText>
+                  <MDBCardText>
+                    My personal website. ReactJS front-end with node/express back-end. Much better than the last one! I also have a pure HTML/CSS/JS version.
+                    SamBurkett.me does not currently support secure connection due to being hosted on Heroku free. I will link the secure connection link below.
+                  </MDBCardText>
                   <MDBBtn color="dark" href="https://github.com/Lee7b/sbappv3">View Project</MDBBtn>
-                  <MDBBtn color="dark">View Live Demo</MDBBtn>
+                  <MDBBtn color="dark" href="https://sbpersonal.herokuapp.com">View Live Demo</MDBBtn>
                 </MDBCardBody>
               </MDBCard>
               </MDBCol>
 
               <MDBCol className="d-flex justify-content-center">
               <MDBCard style={{width:"500px"}}>
-                <MDBCardHeader><strong>sbappv2</strong></MDBCardHeader>
+                <MDBCardHeader><strong>sbappv1</strong></MDBCardHeader>
                 <MDBCardBody>
-                  <MDBCardText>My personal website built with HTML/CSS/JS.</MDBCardText>
-                  <MDBBtn color="dark" href="https://github.com/Lee7b/lee7b.github.io">View Project</MDBBtn>
-                  <MDBBtn color="dark" href="https://sbappv2.herokuapp.com/">View Live Demo</MDBBtn>
+                  <MDBCardText>This was my first website that I created. ReactJS front-end with node/express back-end. Has a MongoDB database. Less fancy.</MDBCardText>
+                  <MDBBtn color="dark" href="https://github.com/Lee7b/sbappold">View Project</MDBBtn>
+                  <MDBBtn color="dark" href="https://sbreactapp.herokuapp.com/">View Live Demo</MDBBtn>
                 </MDBCardBody>
               </MDBCard>
               </MDBCol>
@@ -34,21 +37,20 @@ class Projects extends React.Component {
           <MDBRow>
               <MDBCol className="d-flex justify-content-center">
               <MDBCard style={{width:"500px"}}>
-                <MDBCardHeader><strong>SBX Photography</strong></MDBCardHeader>
+                <MDBCardHeader><strong>MEAN Stack Auth App</strong></MDBCardHeader>
                 <MDBCardBody>             
-                  <MDBCardText>Photography site template built with ReactJS</MDBCardText>
-                  <MDBBtn color="dark">View Project</MDBBtn>
-                  <MDBBtn color="dark" href="https://sbxphotoapp.herokuapp.com/">View Live Demo</MDBBtn>
+                  <MDBCardText>MEAN Stack Authentication App</MDBCardText>
+                  <MDBBtn color="dark" href="https://github.com/Lee7b/MEANStackAuthApp">View Project</MDBBtn>
                 </MDBCardBody>
               </MDBCard>
               </MDBCol>
 
               <MDBCol className="d-flex justify-content-center">
               <MDBCard style={{width:"500px"}}>
-                <MDBCardHeader><strong>Rock paper scissors game</strong></MDBCardHeader>
+                <MDBCardHeader><strong>Java Flappy Bird Clone</strong></MDBCardHeader>
                 <MDBCardBody>                 
-                  <MDBCardText>Rock paper scissors game built with ReactJS</MDBCardText>
-                  <MDBBtn color="dark" href="http://sbreactapp.herokuapp.com/rpsgame">View Project</MDBBtn>
+                  <MDBCardText>Flappy bird clone written in Java. This was a project I did while I was in school.</MDBCardText>
+                  <MDBBtn color="dark" href="https://github.com/Lee7b/javaFlappyGame">View Project</MDBBtn>
                 </MDBCardBody>
               </MDBCard>
               </MDBCol>
